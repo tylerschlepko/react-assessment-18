739cd5af7b5ac5eb4cd9acbc279cf4f59187bf2b
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2023-02-23 10:00:38 -0600

    finished

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 const Comments = () => {
     // Insturction:
     // Use endpoint: https://jsonplaceholder.typicode.com/comments to get a list of comments. 
@@ -7,9 +9,30 @@ const Comments = () => {
     // you will need to use the useEffect hook. Do this in async / await syntax.
 
 
+    const [comments, setComments] = useState([])
+    const getComments = async () =>{
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/comments`)
+            const data = await response.json()
+            setComments(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleClick = (id) => {
+        console.log(id);
+    }
+
+    useEffect(()=>{
+        getComments()
+    },[])
+
     return(
-        // Replace this with your code.
-        <h1>Comments component</h1>
+        <ul>
+            <h1>Comments</h1>
+            {comments.map(comment=><li onClick={()=>{handleClick(comment.id)}} key={comment.id}>{comment.body}</li>)}
+        </ul>
     )
 }
 
--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -10,7 +10,7 @@ const Instructions = () => {
                     <li>B. Associated with class based components</li>
                     <li>C. Associated with function based components</li>
                 </ul>
-                {/* write your answer here =>          */}
+                {/* write your answer here =>    B      */}
             </div>
             <div className="question">
                 <p>If I were wanting to make an API call when the application loads, what hook might I use in a function based component?</p>
@@ -20,7 +20,7 @@ const Instructions = () => {
                     <li>C. componentWillMount</li>
                     <li>C. useState</li>
                 </ul>
-                {/* write your answer here =>          */}
+                {/* write your answer here =>     A      */}
             </div>
             <div className="question">
                 <p>What is the design pattern for using local state within a function component to handle input from a user?</p>
@@ -30,7 +30,7 @@ const Instructions = () => {
                     <li>C. Data validation</li>
                     <li>C. Input validation</li>
                 </ul>
-                {/* write your answer here =>          */}
+                {/* write your answer here =>     B     */}
             </div>
             <div className="question">
                 <p>React has multi-directional data flow.</p>
@@ -38,7 +38,7 @@ const Instructions = () => {
                     <li>A. True</li>
                     <li>B. False</li>
                 </ul>
-                {/* write your answer here =>          */}
+                {/* write your answer here =>    A      */}
             </div>
             <div className="question">
                 <p>React was created and is upkept by</p>
@@ -48,7 +48,7 @@ const Instructions = () => {
                     <li>C. Ecmascript team</li>
                     <li>C. Microsoft</li>
                 </ul>
-                {/* write your answer here =>          */}
+                {/* write your answer here =>      B     */}
             </div>
             <div className="question">
                 <p>Which of the concepts below is not specific to ReactJS</p>
@@ -57,7 +57,7 @@ const Instructions = () => {
                     <li>B. Props</li>
                     <li>C. VirtualDOM</li>
                 </ul>
-                {/* write your answer here =>          */}
+                {/* write your answer here =>    A      */}
             </div>
             <div className="question">
                 <p>Nested components are also called child components</p>
@@ -65,7 +65,7 @@ const Instructions = () => {
                     <li>A. True</li>
                     <li>B. False</li>
                 </ul>
-                {/* write your answer here =>          */}
+                {/* write your answer here =>     A     */}
             </div>
             <div className="question">
                 <p>Nested components are also called child components</p>
@@ -73,7 +73,7 @@ const Instructions = () => {
                     <li>A. True</li>
                     <li>B. False</li>
                 </ul>
-                {/* write your answer here =>          */}
+                {/* write your answer here =>      A     */}
             </div>
 
             <div id="instructionsCopy">Now that you are finished with this, navigate to Comments.js file / component and read instruction there.</div>
